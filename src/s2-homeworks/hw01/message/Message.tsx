93600c00d6f190a2ab6a6176b769ee75b6d36c75
Dzianis Klopat <denisklopot0@gmail.com>
2023-01-12 00:55:12 +0300

    th

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -31,6 +31,7 @@ const Message = (props: MessagePropsType) => {
                     </pre>
                 </div>
             </div>
+            <span className={s.ugol} />
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
                     {props.message.message.time}
